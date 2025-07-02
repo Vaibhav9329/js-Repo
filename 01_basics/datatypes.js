4ee42bd756ad4f528ca9adde141150d9e96a55fd
vaibhav  Khot <vkhot9329@gmail.com>
2025-07-02 16:00:32 +0000

    variables and datatypes is done today

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict";  // treat all js code newer version
+
+// alert(3+3)  //we are using nodejs, not browser
+
+console.log(3+3);
+// code readibality should be high
+console.log("vaibhav")
+
+let name = "vaibhav"  
+let age = 20
+let isLoggedIn = false
+let state =""
+let city = null
+
+// number => 2 to pwr 53
+// bigint =>  used in big numbers
+// String => ""
+// bollean => true/false
+// null => standalone value / also it is abject
+// undefined => value that unsigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof null)
