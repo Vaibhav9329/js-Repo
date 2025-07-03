02239559c52ac57d1637e180661f36c91c304916
vaibhav  Khot <vkhot9329@gmail.com>
2025-07-03 18:44:45 +0000

    Conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,28 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+
+let isloggedIn = "vaibhav"
+let booleanIsLoggedIn = Boolean(isloggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false; "String" => true;
+
+let sumNumber = 33
+let stringNumber = String(sumNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
