9b9509e2797d2d4087270b45947822ed7e9d469c
vaibhav  Khot <vkhot9329@gmail.com>
2025-07-07 14:11:36 +0000

    String is done

---
+++ 01_basics/String.js
@@ -0,0 +1,28 @@
+const name = "vaibhav"
+const repo = 50
+
+console.log(name + repo+"value")
+
+console.log(`hello my name is ${name} and my repo cout is ${repo}`)
+
+const gameName = new String ("hitesh")
+
+console.log(gameName[0]);
+console.log(gameName._proto_);
+
+console.log(gameName.length);
+console.log(gameName.charAt(5));
+console.log(gameName.toUpperCase());
+console.log(gameName.split());
+console.log(gameName.substring(2,5));
+
+const newStringone = "  vaibhav   "
+console.log(newStringone)
+console.log(newStringone.trim())
+
+
+const url ="https://vaibhav.com/vaibhav%20khot"
+
+console.log(url.replace('%20','-'))
+console.log(url.includes('%20'))
+console.log(gameName.split('-'))
